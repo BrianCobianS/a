a7c09c0746d26ba479dd763980eb774af4d5c7f6
brian.cobian <brian.cobian@toshibagcs.com>
2022-11-18 21:40:39 -0600

    Modification in now

--- Now.js
+++ Now.js
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 function now(Controlador){
 
-   exec(`python3  /home/ebossteam/UnattendedInstallation/FULLSTACK/Back-End/public/Inventories/now.py ${Controlador.ip} ${Controlador.usr} ${Controlador.pass} ${Controlador.opc} ${Controlador.complemento} ${Controlador.nivel} ${Controlador.ASM} ${Controlador.fecha} ${Controlador.version} ${Controlador.paquetes} ${Controlador.email}`, (error, stdout, stderr) => {
+   exec(`python3  /home/ebossteam/UnattendedInstallation/FULLSTACK/Back-End/public/Inventories/now.py ${Controlador.ip} ${Controlador.usr} ${Controlador.pass} ${Controlador.opc} ${Controlador.complemento} ${Controlador.nivel} ${Controlador.ASM} ${Controlador.fecha} ${Controlador.version} ${Controlador.email}`, (error, stdout, stderr) => {
       if (error) {
       console.error(`error: ${error.message}`);
       return;
