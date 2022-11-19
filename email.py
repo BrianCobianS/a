e019466052772e237e21c315259753d8ef7494e8
brian.cobian <brian.cobian@toshibagcs.com>
2022-11-19 16:20:55 -0600

    Modification in now

--- email.py
+++ email.py
@@ -8,8 +8,10 @@ ASM=(sys.argv[6])
 FECHA=(sys.argv[7])
 VERSION=(sys.argv[8])
 EMAIL=(sys.argv[9])
+#pruebas  python .\email.py 12.12.12 master m1 1 12.ver12 accept 12712/12 Morty brian.cobian@toshibagcs.com
 print(IP,USR,PASS,OPC,LEV,ASM,FECHA,VERSION,EMAIL)
-def createmail(message):
+def createmail(message,path,boleano):
+    # contenido = open("C:/Users/brian.cobian/Desktop/IMDesatendida/Fullstackpage/mails/email.txt").read().splitlines()
     contenido = open("/home/ebossteam/UnattendedInstallation/FULLSTACK/mails/email.txt").read().splitlines()
     contenido.insert(2,"    ip: '"+IP+"',")
     contenido.insert(2,"    usr: '"+USR+"',")
@@ -22,8 +24,11 @@ def createmail(message):
     contenido.insert(2,"    fecha: '"+FECHA+"',")
     contenido.insert(2,"    opc: '"+OPC+"',")
     contenido.insert(2,"    message: '"+message+"',")
+    contenido.insert(2,"    path: '"+path+"',")
+    contenido.insert(2,"    boleano: '"+str(boleano)+"',")
     f = open('/home/ebossteam/UnattendedInstallation/FULLSTACK/mails/mail.js', "w")
-    #pruebas  python .\email.py 12.12.12 master m1 1 brian.cobian@alumnos.udg.mx 12.ver12 accept Morty ACE3DYEPS 12712/12
+    # f = open('C:/Users/brian.cobian/Desktop/IMDesatendida/Fullstackpage/mails/mail.js', "w")
+    
     f.writelines("\n".join(contenido))
     f.close
 
@@ -32,17 +37,17 @@ def changeBat(fileDirectory):
         ban = 0
         i=0
         Exito=0
-        Fallo =0
         reserva=[]
-        Error='The installation failed, the error occurred at:\n '
-        Succes= 'The installation was successful, the current controller version is:\n'
+        Error='The installation failed, the error occurred at \t\t\t'
+        Succes= 'The installation was successful, the current controller version is\t\t\t'
+        
         for linea in archivo:
             linea=linea.rstrip('\n')
             reserva.append(linea)
             if 'fatal:' in  linea:
                  for linea in reserva[i-3:i+1]:
                     Error=Error+linea
-                 Fallo=1
+                 Exito=0
             elif 'SUCCES' in linea:
                  Exito=1
             if 'Print RML' in linea or ban==1:
@@ -51,14 +56,23 @@ def changeBat(fileDirectory):
             i += 1
     if Exito ==1:
         print(Succes)
-        createmail(Succes)
-    elif Fallo==1:
+        createmail(Succes,fileDirectory,Exito)
+    elif Exito==0:
         print(Error)
-        createmail(Error)
+        createmail(Error,fileDirectory,Exito)
     else:
         print('No se sabe que sucedio revisar log')
-        createmail('No se sabe que sucedio revisar log')
-    
+        createmail('No se sabe que sucedio revisar log',fileDirectory,Nose=-1)
+# changeBat(str(x))
+
+x= open("/home/ebossteam/temp/"+IP+"/date.txt",'r')
+for x in x:
+    date=x
+print(date)
+changeBat("/var/log/logscontroladores/"+IP+"-"+date+".txt")
+
+# changeBat("C:/Users/brian.cobian/Desktop/IMDesatendida/Fullstackpage/mails/log.txt")
+
 
 
 
